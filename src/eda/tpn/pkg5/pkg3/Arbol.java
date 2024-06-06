787752ab7f61e10f03c4f9f374cb9396cb557f75
BlasTobares <163307394+BlasTobares@users.noreply.github.com>
2024-06-06 19:03:01 -0300

    cambios

--- src/eda/tpn/pkg5/pkg3/Arbol.java
+++ src/eda/tpn/pkg5/pkg3/Arbol.java
@@ -4,12 +4,23 @@ package eda.tpn.pkg5.pkg3;
 public class Arbol {
     
     private Nodo raiz;
-
+    private Integer vector[] = new Integer [12];
+    
     public Arbol() {
         this.raiz = null;
     }
     
+    public void cargaVectorPorDefecto(){
+        
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = -1;
+            
+        }
+        
+    }
     
+    
+    // Implementacion enlazada
     public Nodo insertarN(Nodo raiz, int valor) {
 
         if (raiz == null) {
@@ -28,21 +39,23 @@ public class Arbol {
     }
     
     
-    public void recorridoPreorden(Nodo nodo) {
+    public void preOrden(Nodo nodo) {
+
         if (nodo == null) {
-            return;
+            return; //detiene recursividad
+            
+        } else {
+            System.out.print(nodo.getValor() + " ");
+            preOrden(nodo.getIzquierdo());
+            preOrden(nodo.getDerecho());
         }
 
-        // Visita el nodo raíz
-        System.out.print(nodo.valor + " ");
-
-        // Recorre el subárbol izquierdo
-        recorridoPreorden(nodo.izquierdo);
-
-        // Recorre el subárbol derecho
-        recorridoPreorden(nodo.derecho);
     }
     
+    
+    public void recorrerPreOrden(Nodo raiz) {
+        preOrden(raiz); // Inicia el recorrido desde la raíz
+    }
     
     
     public void busqueda(Nodo nodo, int valor) {
@@ -62,11 +75,37 @@ public class Arbol {
         }
 
         if (punt == null) {
-            System.out.println("No se encontro el ingrediente [" + valor + "]");
+            System.out.println("---> No se encontro");
         } else {
-            System.out.println("Se encontro el ingrediente [" + valor + "] buscado");
+            System.out.println("---> Se encontro");
         }
 
     }
     
+    
+    // Método para guardar los elementos del árbol en el vector de forma correspondiente
+    public void guardarEnVector(Nodo raiz) {
+        guardarEnVectorRecursivo(raiz, 0);
+    }
+
+    // Método de iteracion recursiva
+    private void guardarEnVectorRecursivo(Nodo nodo, int indice) {
+        if (nodo != null) {
+            vector[indice] = nodo.getValor(); // Guardar el valor en el índice actual
+            guardarEnVectorRecursivo(nodo.getIzquierdo(), (2 * indice + 1)); // Llamada recursiva para el hijo izquierdo
+            guardarEnVectorRecursivo(nodo.getDerecho(), (2 * indice + 2)); // Llamada recursiva para el hijo derecho
+        }
+    }
+    
+    
+    public void visualizarVector(){
+        
+        for (int i = 0; i < vector.length-1; i++) {
+            
+            System.out.print("["+vector[i]+"]");
+            
+        }
+        
+        System.out.println(" ");
+        } 
 }
--- src/eda/tpn/pkg5/pkg3/Nodo.java
+++ src/eda/tpn/pkg5/pkg3/Nodo.java
@@ -17,6 +17,31 @@ public class Nodo {
         this.derecho = null;
     }
 
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Nodo getIzquierdo() {
+        return izquierdo;
+    }
+
+    public void setIzquierdo(Nodo izquierdo) {
+        this.izquierdo = izquierdo;
+    }
+
+    public Nodo getDerecho() {
+        return derecho;
+    }
+
+    public void setDerecho(Nodo derecho) {
+        this.derecho = derecho;
+    }
+    
+
     @Override
     public String toString() {
         return String.valueOf(valor);
